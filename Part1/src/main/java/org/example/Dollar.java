b76cafd14b63e831d214a058ae03c2d7b175decf
woaml <set8el1@gmail.com>
2024-09-17 22:20:35 +0300

    finished 3. Equality for all.

--- Part1/src/main/java/org/example/Dollar.java
+++ Part1/src/main/java/org/example/Dollar.java
@@ -12,4 +12,8 @@ public class Dollar {
         return new Dollar(amount * multiplier);
     }
 
+    public boolean equals(Object object){
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
 }
--- Part1/src/test/java/MyClassTest.java
+++ Part1/src/test/java/MyClassTest.java
@@ -1,7 +1,7 @@
 import org.example.Dollar;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class MyClassTest {
 
@@ -12,5 +12,11 @@ public class MyClassTest {
         assertEquals(10, product.amount);
         product = five.times(3);
         assertEquals(15, product.amount);
+    }
+
+    @Test
+    public void testEquality(){
+        assertTrue(new Dollar(5).equals(new Dollar(5)));
+        assertFalse(new Dollar(5).equals(new Dollar(6)));
     }
 }
