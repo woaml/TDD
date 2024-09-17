e3c0510198597291ebe7211f5fe21cb9e68ef4a7
woaml <set8el1@gmail.com>
2024-09-17 22:33:58 +0300

    finished 5. Let's talk about francs

--- Part1/src/test/java/MyClassTest.java
+++ Part1/src/test/java/MyClassTest.java
@@ -1,4 +1,5 @@
 import org.example.Dollar;
+import org.example.Franc;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,6 +11,13 @@ public class MyClassTest {
         Dollar five = new Dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
+    }
+
+    @Test
+    public void testFrancMultiplication(){
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
     }
 
     @Test
