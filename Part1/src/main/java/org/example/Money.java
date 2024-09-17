eac55eb13e19c07f19743cd15476733139421f20
woaml <set8el1@gmail.com>
2024-09-17 23:12:38 +0300

    finished 7. Creating objects

--- Part1/src/main/java/org/example/Money.java
+++ Part1/src/main/java/org/example/Money.java
@@ -1,11 +1,21 @@
 package org.example;
 
-public class Money {
+public abstract class Money {
     protected int amount;
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
 
     public boolean equals(Object object){
         Money money = (Money) object;
         return amount == money.amount && getClass().equals(money.getClass());
     }
+
+    public static Money dollar(int amount){
+        return new Dollar(amount);
+    }
+
+    public abstract Money times(int multiplier);
 
 }
