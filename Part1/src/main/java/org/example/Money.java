d72145b2432e814c4a9cae8b940ecdef283cc3ff
woaml <set8el1@gmail.com>
2024-09-18 09:32:06 +0300

    finished 15. Mixing currencies

--- Part1/src/main/java/org/example/Money.java
+++ Part1/src/main/java/org/example/Money.java
@@ -23,7 +23,7 @@ public class Money implements Expression{
         return amount == money.amount && currency().equals(money.currency());
     }
 
-    public Money times(int multiplier){
+    public Expression times(int multiplier){
         return new Money(amount * multiplier, currency);
     }
 
@@ -31,7 +31,7 @@ public class Money implements Expression{
         return currency;
     }
 
-    public Expression plus(Money addend) {
+    public Expression plus(Expression addend) {
         return new Sum(this, addend);
     }
 
